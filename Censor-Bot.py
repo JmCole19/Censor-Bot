7a61aba8f2d8862d019a65c6f6a495f1f9848787
Jason Coleman <jayemcee121@gmail.com>
2020-12-01 04:50:56 -0500

    Finishing up, comments are next

--- Censor-Bot.py
+++ Censor-Bot.py
@@ -1,45 +1,39 @@
 import tweepy
 from credentials import *
 from time import sleep
+import schedule
 
 auth = tweepy.OAuthHandler(ckey, csecret)
 auth.set_access_token(akey, asecret)
 api = tweepy.API(auth)
 
-screen_name = "sinjinwest"
-
 userId = 4281356421
 
-targetTimeline = api.user_timeline(userId, count=1)
+targetTimeline = api.user_timeline(userId, count=25)
 
 prompt = open('Prompt.txt', 'r')
 prompt_lines = prompt.readlines()
 prompt.close()
 
 for status in targetTimeline:
-	explicitContent = ["fuck", "shit", "sex", "hoe", "ass", "onlyfans", "horny", "dick", "tits", "pussy"]
-	
-	tweetText = status.text.lower()
-	
-	print(str(status.user.name + ': ' + status.text + '- Tweet ID: ' + str(status.id)))
-	
+	explicitContent = ["fuck", "shit", "sex", "hoe", "ass", "onlyfans", "horny", "dick", "tits", "pussy", "nude", "nudes"]
+	tweetText = status.text.lower()	
 	targetTweet = status.id
-	
+
+	print(str(status.user.name + ': ' + status.text + ' - Tweet ID: ' + str(status.id)))
+		
 def tweet():
 	for line in prompt_lines:
 		try:
-			if line != '\n':
-				print(line)
-				sleep(300)
+			if any(s in tweetText for s in explicitContent):
+				api.update_status(str("Hello @" + status.user.screen_name + ',' + line), targetTweet)
+				sleep(3)
 			else:
 				pass
 			
 		except tweepy.TweepError as e:
 				print(e.reason)
 			
-			
-	sleep(2)
-		
-	if any(s in tweetText for s in explicitContent):
-		tweet()
+	sleep(10)
 
+tweet()
